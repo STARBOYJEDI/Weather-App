b66322ff375d0cc50e89f196eeaa49918220adb9
Mbuso Mabuza <mbusomabuza0613@gmail.com>
2026-04-13 14:53:47 +0200

    fix: correct formatting of parameters in getWeather function

--- weather.js
+++ weather.js
@@ -6,12 +6,13 @@ export function getWeather(lat, lon, timezone) {
     return axios.get(
         "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&hourly=temperature_2m,apparent_temperature,precipitation_probability,weather_code,wind_speed_10m&timezone=Africa%2FCairo&timeformat=unixtime", 
         { 
-        params: {
-            latitude: lat,
-            longitude: lon,
-            timezone,
-        },
-    })
+            params: {
+                latitude: lat,
+                longitude: lon,
+                timezone,
+            },
+        }
+    )
 }
 
 
